6a577bc7f4196e5855732aed29ee6489ec2ab5e7
cp <1511247239@qq.com>
2019-09-18 16:04:22 +0800

    主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="home">
-    我是主页
-  </div>
+<!-- 定义一个大容器 -->
+  <el-container>
+      <!-- 左侧导航 -->
+      <el-aside>左侧内容</el-aside>
+      <!-- 右侧大容器 -->
+      <el-container>
+          <!-- 右侧头部 -->
+          <el-header>头部</el-header>
+          <!-- 中部区域 -->
+          <el-main>
+              <!-- 二级路由 -->
+              <router-view></router-view>
+          </el-main>
+      </el-container>
+  </el-container>
 </template>
 
 <script>
