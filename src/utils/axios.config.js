f6f90b36059f6a20eb537c2bbdf0e1e8b30ec608
cp <1511247239@qq.com>
2019-09-20 16:29:49 +0800

    添加响应拦截器

--- src/utils/axios.config.js
+++ src/utils/axios.config.js
@@ -1,10 +1,18 @@
 import axios from 'axios'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+// 请求拦截
 axios.interceptors.request.use(function (config) {
   // 在发起请求请做一些业务处理
   let token = window.localStorage.getItem('user-token')
   config.headers['Authorization'] = `Bearer ${token}`
   return config
+}, function (error) {
+  // 对请求失败做处理
+  return Promise.reject(error)
+})
+// 响应拦截
+axios.interceptors.response.use(function (response) {
+  return response.data ? response.data : {}
 }, function (error) {
   // 对请求失败做处理
   return Promise.reject(error)
